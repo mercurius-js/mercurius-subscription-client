42c6dcbb2eeb0760e5b9d15c086ada1d3a5b0c58
Davide Fiorello <davide@codeflyer.com>
2022-12-09 12:51:12 +0100

    feat: import from mercurius

---
+++ test/types/index.ts
@@ -0,0 +1,33 @@
+import { MercuriusContext } from 'mercurius'
+import { GRAPHQL_TRANSPORT_WS_PROTOCOL } from 'graphql-ws'
+
+import { SubscriptionClient } from '../..'
+
+const subscriptionClient = new SubscriptionClient('ws://localhost', {
+  protocols: [GRAPHQL_TRANSPORT_WS_PROTOCOL],
+  reconnect: false,
+  maxReconnectAttempts: 10,
+  serviceName: 'sample-name',
+  connectionCallback: async () => {
+  },
+  failedConnectionCallback: async () => {
+  },
+  failedReconnectCallback: async () => {
+  },
+  connectionInitPayload: {},
+  rewriteConnectionInitPayload: {},
+  keepAlive: true
+})
+
+subscriptionClient.connect()
+subscriptionClient.reconnect()
+subscriptionClient.close(true)
+subscriptionClient.getReconnectDelay()
+subscriptionClient.unsubscribeAll()
+subscriptionClient.sendMessage('opId', 'test', {}, {})
+subscriptionClient.handleMessage({ foo: 'bar' })
+subscriptionClient.startOperation('opId')
+subscriptionClient.createSubscription('query', {}, async () => {
+}, {} as MercuriusContext)
+subscriptionClient.startKeepAliveInterval()
+subscriptionClient.stopKeepAliveInterval()
