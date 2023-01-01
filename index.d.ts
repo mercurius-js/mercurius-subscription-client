ecc72ed143fb3044925d0768f53c7270d511ca3b
Marco Ippolito <marcoippolito54@gmail.com>
2023-01-01 19:43:01 +0100

    test: types tests

--- index.d.ts
+++ index.d.ts
@@ -2,16 +2,16 @@ import { MercuriusContext } from "mercurius";
 import { EventEmitter } from "stream";
 
 export interface SubscriptionClientConfig {
-  protocols: Array<string>,
-  reconnect: boolean,
-  maxReconnectAttempts: number,
+  protocols?: Array<string>,
+  reconnect?: boolean,
+  maxReconnectAttempts?: number,
   serviceName: string,
-  connectionCallback: (params: any) => Promise<void>,
-  failedConnectionCallback: (params: any) => Promise<void>,
-  failedReconnectCallback: (params: any) => Promise<void>,
-  connectionInitPayload: any,
-  rewriteConnectionInitPayload: (connectionInit: any, context: any) => void,
-  keepAlive: number
+  connectionCallback?: (params: any) => Promise<void>,
+  failedConnectionCallback?: (params: any) => Promise<void>,
+  failedReconnectCallback?: (params: any) => Promise<void>,
+  connectionInitPayload?: any,
+  rewriteConnectionInitPayload?: (connectionInit: any, context: MercuriusContext) => void,
+  keepAlive?: number
 }
 
 export type SubscriptionOperationId = string
@@ -21,7 +21,7 @@ export class SubscriptionClient extends EventEmitter {
 
   connect(): void
   close (tryReconnect: boolean, closedByUser?: boolean): void
-  unsubscribe (operationId: SubscriptionOperationId, forceUnsubscribe: boolean) : void
+  unsubscribe (operationId: SubscriptionOperationId, forceUnsubscribe?: boolean) : void
   unsubscribeAll() : void
   createSubscription (query: string, variables: any, publish: (props: any) => Promise<void>, context: MercuriusContext) : SubscriptionOperationId
 }
--- test/types/index.ts
+++ test/types/index.ts
@@ -1,9 +1,9 @@
 import { MercuriusContext } from 'mercurius'
 import { GRAPHQL_TRANSPORT_WS_PROTOCOL } from 'graphql-ws'
+import { expectAssignable, expectType } from 'tsd'
+import { SubscriptionClient, SubscriptionClientConfig, SubscriptionOperationId } from '../..'
 
-import { SubscriptionClient } from '../..'
-
-const subscriptionClient = new SubscriptionClient('ws://localhost', {
+const config = {
   protocols: [GRAPHQL_TRANSPORT_WS_PROTOCOL],
   reconnect: false,
   maxReconnectAttempts: 10,
@@ -18,10 +18,22 @@ const subscriptionClient = new SubscriptionClient('ws://localhost', {
   rewriteConnectionInitPayload: () => {
   },
   keepAlive: 1000
+}
+
+expectAssignable<SubscriptionClientConfig>(config)
+
+expectAssignable<SubscriptionClientConfig>({
+  serviceName: 'test'
 })
 
-subscriptionClient.connect()
-subscriptionClient.close(true)
-subscriptionClient.unsubscribeAll()
-subscriptionClient.createSubscription('query', {}, async () => {
+const subscriptionClient = new SubscriptionClient('ws://localhost', config)
+
+expectType<void>(subscriptionClient.connect())
+expectType<void>(subscriptionClient.close(true))
+expectType<void>(subscriptionClient.unsubscribeAll())
+
+const subscription = subscriptionClient.createSubscription('query', {}, async () => {
 }, {} as MercuriusContext)
+
+expectType<SubscriptionOperationId>(subscription)
+expectType<void>(subscriptionClient.unsubscribe(subscription))
